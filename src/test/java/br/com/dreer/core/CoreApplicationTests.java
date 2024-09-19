e84717f62ca9401152f88daeaf0da84af5f9f539
Dogulas Dreer <douglasdreer@outlook.com.br>
2024-09-18 22:18:02 -0300

    Initial commit

---
+++ src/test/java/br/com/dreer/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dreer.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
